2cdc566fe2644c6da57b7dc6713d997e59e97f25
emorton <elena.uleia@gmail.com>
2017-07-31 11:20:58 +0100

    starting refactoring in ruby style

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -12,29 +12,28 @@ class Crawler
 
 	def fetch_data
 		create_csv_file
-		count = 0
-		@seeds.each do |seed|
-			count += 1
+		id = 0
+		@seeds.map do |seed|
 			checked_seed = check_url_or_file(seed)
 
-			id = count 
 			urls = fetch_urls(checked_seed)
 			keywords = fetch_metadata('keywords', checked_seed)
 			description = fetch_metadata('description',checked_seed)
 			headers = fetch_headers(checked_seed)
-			text = fetch_paragraphs(checked_seed)		
+			text = fetch_paragraphs(checked_seed)
+
+			id += 1		
 			seed_data = SeedData.new(id, seed, urls, keywords, description, headers, text)
 			seed_data.store_in_csv
 		end
 	end
 
 	def fetch_urls(seed)
-		urls = ""
 		seed_urls_nodeset = seed.xpath('//a')
-		seed_urls_nodeset.each do |node|
-			urls += node.first[1] + " " if node.first[1].include?('http') #|| node.first.include?('https://')
-		end
-		return urls.strip
+
+		seed_urls_nodeset.select { |node| node.first[1].include?('http') }
+		.inject(""){ |node| node.first[1] + " " } #|| node.first.include?('https://')
+		.strip
 	end
 
 	def fetch_metadata(attribute, seed)
@@ -42,10 +41,10 @@ class Crawler
 		get_from_nodeset(attribute, meta_from_nodeset)
 	end
 
-		def fetch_paragraphs(seed)
+	def fetch_paragraphs(seed)
 		full_text = ""
 		seed_paragraph_nodeset = seed.xpath('//p')
-		seed_paragraph_nodeset.each do |node|
+		seed_paragraph_nodeset.map do |node|
 			raw_text = node.text.delete('^A-Za-z ')
 			raw_text.gsub!(/[\n,\t]/, " ") if raw_text.include?('\n')
 			full_text += "#{raw_text} "
@@ -56,7 +55,8 @@ class Crawler
 	def fetch_headers(seed)
 		headers = ""
 		headers_tags = (1..6).map { |num| "h#{num}"}
-		headers_tags.each do |header_tag|
+
+		headers_tags.map do |header_tag|
 			tag_header = get_header_from_tag(seed, header_tag)
 			headers += tag_header + " " if tag_header
 		end
@@ -75,7 +75,7 @@ class Crawler
 		end
 	end
 
-		def get_from_nodeset(attribute, nodeset)
+	def get_from_nodeset(attribute, nodeset)
 		nodeset.each do |node|
 			next unless node.attributes['name']
 			output = node.attributes['content'].value
