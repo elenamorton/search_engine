663044d49e7d52a68621b2c37babb1fe39c88216
Elena Morton <elena.uleia@gmail.com>
2017-08-01 16:08:42 +0000

    replaced hard coded file name

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'csv'
 require_relative 'crawler'
 require_relative 'ranking'
@@ -15,14 +17,15 @@ puts ascii_spider
 
 #gets input from User
 puts "Enter search keyword"
-query = gets.downcase.chomp
-puts "You have entered: " + query
+@query = gets.downcase.chomp
+puts "You have entered: " + @query
 puts ""
 
 list = []
+
 #creates Rankers, stores them in array
-indexer.web_pages.each do |page_hash|
-	ranker = Ranker.new(page_hash, query)
+indexer.web_pages.each() do |page_hash|
+	ranker = Ranker.new(page_hash, @query)
 	ranker.calculate_total_score
 	list << ranker
 end
@@ -34,9 +37,9 @@ end
 
 #returns info from CSV file relevant to seed in ranker
 puts "Results:"
-sorted_list.each do |ranker|
-	CSV.foreach('seeddata.csv', col_sep: "|", quote_char: "|", headers: true) do |row|
+sorted_list.map do |ranker|
+	CSV.foreach(DATA_FILE, col_sep: "|", quote_char: "|", headers: true) do |row|
 		row[4] = "Description not available" if row[4] == "0"
-    puts "#{row[1]} \n #{row[4]}" if row[0] == ranker.hash[:id].keys[0]
+    	puts "#{row[1]} \n #{row[4]}" if row[0] == ranker.hash[:id].keys[0]
 	end
 end
