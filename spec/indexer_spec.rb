d4ad5600d601d4599e2e40c0a9d1fcdc7f44feed
Elena Morton <elena.uleia@gmail.com>
2017-08-01 15:17:59 +0000

    rename indexer state var.

--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -2,8 +2,12 @@ require 'indexer.rb'
 
 describe Indexer do
 
-  let(:indexer) {Indexer.new}
-  let(:file) {double("file")}
+  let(:indexer) { Indexer.new }
+  csv =  __dir__ + '/indexer_test.csv'
+
+#	CSV.foreach(csv, col_sep: "|", quote_char: "|", headers: true) do |row|
+#  	let(:page_indexer) { PageIndexer.new(row) }
+#	end
 
   describe 'initializes the indexer' do
     it 'exists' do
@@ -12,9 +16,16 @@ describe Indexer do
   end
 
   describe 'reads the seedata file' do
-    xit 'reads a csv file' do
-      allow('file').to receive(:open).with('r')
-      expect(indexer.read_csv).to
+    it 'reads a csv file' do
+      indexer.process_csv(csv)
+      expect(indexer.web_pages).not_to be_empty
     end
+    
+    it 'processes a web page from a csv file for :headers' do
+      indexer.process_csv(csv)
+      expect(indexer.web_pages.first[:headers]).to eq ({"raven"=>1})
+    end
+    
   end
+  
 end
